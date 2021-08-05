ed3569a5f21f2052d36fccf0be96afe3d656bab1
bojack <695668678@qq.com>
2021-08-05 14:46:00 +0800

    feat : add login&register&logout

---
+++ src/pages/Index/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { useAuth } from "context/auth_context";
+import { Button } from "antd";
+const Index = () => {
+  const { user, logout } = useAuth();
+  return (
+    <>
+      <div>欢迎你{user?.name}</div>
+      <Button onClick={logout}>登出</Button>
+    </>
+  );
+};
+export default Index;
