8f51d14133dfef38ef70b0db13a1c1bebde2f344
bojack <695668678@qq.com>
2021-08-07 15:59:30 +0800

    feat : 优化header

--- src/pages/Index/components/HeaderPro.tsx
+++ src/pages/Index/components/HeaderPro.tsx
@@ -1,14 +1,20 @@
 import styled from "@emotion/styled";
 import { Button, Popover } from "antd";
 import { Header } from "antd/lib/layout/layout";
+import { ReactComponent as SoftwareLogo } from "assets/software-logo.svg";
 import { useAuth } from "context/auth_context";
 import React from "react";
 const HeaderPro = () => {
   const { user, logout } = useAuth();
-  const exButton = <Button onClick={logout}>登出</Button>;
+  const exButton = (
+    <Button type="link" onClick={logout}>
+      登出
+    </Button>
+  );
   return (
     <>
       <StyledHeader>
+        <SoftwareLogo width={"18rem"} color={"rgb(38, 132, 255)"} />
         <Popover placement="bottom" content={exButton} trigger="click">
           <Button type="link">Hi,{user?.name}</Button>
         </Popover>
@@ -20,7 +26,7 @@ const StyledHeader = styled(Header)`
   padding: 0 16px;
   background: #fff;
   display: flex;
-  justify-content: flex-end;
+  justify-content: space-between;
   align-items: center;
 `;
 export default HeaderPro;
--- src/pages/Index/index.tsx
+++ src/pages/Index/index.tsx
@@ -13,53 +13,73 @@
 //   );
 // };
 // export default Index;
-import { Layout, Menu } from "antd";
+import { Layout, Menu, Breadcrumb } from "antd";
 import {
-  UploadOutlined,
   UserOutlined,
-  VideoCameraOutlined,
+  LaptopOutlined,
+  NotificationOutlined,
 } from "@ant-design/icons";
 import HeaderPro from "./components/HeaderPro";
-
+const { SubMenu } = Menu;
 const { Content, Footer, Sider } = Layout;
 
 const Index = () => {
   return (
-    <Layout style={{ height: "100vh" }}>
-      <Sider
-        breakpoint="lg"
-        collapsedWidth="0"
-        onBreakpoint={(broken) => {
-          console.log(broken);
-        }}
-        onCollapse={(collapsed, type) => {
-          console.log(collapsed, type);
-        }}
-      >
-        <div className="logo" />
-        <Menu theme="dark" mode="inline" defaultSelectedKeys={["4"]}>
-          <Menu.Item key="1" icon={<UserOutlined />}>
-            nav 1
-          </Menu.Item>
-          <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-            nav 2
-          </Menu.Item>
-          <Menu.Item key="3" icon={<UploadOutlined />}>
-            nav 3
-          </Menu.Item>
-          <Menu.Item key="4" icon={<UserOutlined />}>
-            nav 4
-          </Menu.Item>
-        </Menu>
-      </Sider>
+    <Layout>
+      <HeaderPro />
       <Layout>
-        <HeaderPro />
-        <Content style={{ margin: "24px 16px 0" }}>
-          <div style={{ padding: 24, minHeight: "100vh", background: "#fff" }}>
-            content
-          </div>
-        </Content>
-        <Footer style={{ textAlign: "center" }}>©2021 Created by Bojack</Footer>
+        <Sider width={200} className="site-layout-background">
+          <Menu
+            mode="inline"
+            defaultSelectedKeys={["1"]}
+            defaultOpenKeys={["sub1"]}
+            style={{ height: "100%", borderRight: 0 }}
+          >
+            <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
+              <Menu.Item key="1">option1</Menu.Item>
+              <Menu.Item key="2">option2</Menu.Item>
+              <Menu.Item key="3">option3</Menu.Item>
+              <Menu.Item key="4">option4</Menu.Item>
+            </SubMenu>
+            <SubMenu key="sub2" icon={<LaptopOutlined />} title="subnav 2">
+              <Menu.Item key="5">option5</Menu.Item>
+              <Menu.Item key="6">option6</Menu.Item>
+              <Menu.Item key="7">option7</Menu.Item>
+              <Menu.Item key="8">option8</Menu.Item>
+            </SubMenu>
+            <SubMenu
+              key="sub3"
+              icon={<NotificationOutlined />}
+              title="subnav 3"
+            >
+              <Menu.Item key="9">option9</Menu.Item>
+              <Menu.Item key="10">option10</Menu.Item>
+              <Menu.Item key="11">option11</Menu.Item>
+              <Menu.Item key="12">option12</Menu.Item>
+            </SubMenu>
+          </Menu>
+        </Sider>
+        <Layout style={{ padding: "0 24px 24px" }}>
+          <Breadcrumb style={{ margin: "16px 0" }}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>List</Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
+          </Breadcrumb>
+          <Content
+            className="site-layout-background"
+            style={{
+              padding: 24,
+              margin: 0,
+              minHeight: "80vh",
+              background: "#fff",
+            }}
+          >
+            Content
+          </Content>
+          <Footer style={{ textAlign: "center" }}>
+            Create by Bojack @2021
+          </Footer>
+        </Layout>
       </Layout>
     </Layout>
   );
