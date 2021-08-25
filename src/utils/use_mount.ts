98db6b94ec206220dfb7924fbe0bcc9288863489
bojack <695668678@qq.com>
2021-08-25 17:52:54 +0800

    feat : add useMount

---
+++ src/utils/use_mount.ts
@@ -0,0 +1,9 @@
+import { useEffect } from "react";
+
+export const useMount = (fn: () => void) => {
+  useEffect(
+    () => fn(),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    []
+  );
+};
