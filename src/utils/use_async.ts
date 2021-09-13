530c6bd4b08474700d5265e0303df615cf56d9e9
bojack <695668678@qq.com>
2021-09-13 21:02:14 +0800

    feat : use useReducer to implement useAsync

--- src/utils/use_async.ts
+++ src/utils/use_async.ts
@@ -1,37 +1,65 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useReducer } from "react";
 import { useMountedRef } from "./hooks";
+
+enum StatusType {
+  idle = "idle",
+  pending = "pending",
+  success = "success",
+  error = "error",
+}
+
+interface IState<T> {
+  status: StatusType;
+  value: T | null;
+  error: Error | null;
+  loading: boolean;
+}
+
+const initialState = {
+  status: StatusType.idle,
+  value: null,
+  error: null,
+  loading: false,
+};
 
 export const useAsync = <T>(
   asyncFn: (params?: any) => Promise<T>,
   immediate = false
 ) => {
-  const [status, setStatus] = useState("idle");
-  const [value, setValue] = useState<null>();
-  const [error, setError] = useState<null>();
-  const [loading, setLoading] = useState(false);
+  const [state, dispatch] = useReducer(
+    (state: IState<T>, action: Partial<IState<T>>) => ({ ...state, ...action }),
+    initialState
+  );
+  const { status, value, error, loading } = state;
 
   const mountedRef = useMountedRef();
+
+  const setError = useCallback((error) => {
+    dispatch({ status: StatusType.error, error, loading: false });
+  }, []);
+
+  const setValue = useCallback((value) => {
+    dispatch({ status: StatusType.success, value, loading: false });
+  }, []);
+  const setInit = useCallback(
+    () => dispatch({ status: StatusType.pending, loading: true }),
+    []
+  );
+
   const execute = useCallback(
     (params?) => {
-      setStatus("pending");
-      setLoading(true);
-      setValue(null);
-      setError(null);
+      setInit();
       return asyncFn(params)
         .then((response: any) => {
           if (mountedRef.current) {
             setValue(response);
-            setLoading(false);
-            setStatus("success");
           }
         })
         .catch((error: any) => {
           setError(error);
-          setLoading(false);
-          setStatus("error");
         });
     },
-    [asyncFn, mountedRef]
+    [asyncFn, mountedRef, setInit, setValue, setError]
   );
 
   useEffect(() => {
